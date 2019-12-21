903d8466fa81612eb05b7f0d6f351a651871f576
Lars Willighagen <lars.willighagen@gmail.com>
2019-12-21 17:09:44 +0100

    feat(day-05): add step_input method

--- day-05/intcode.rs
+++ day-05/intcode.rs
@@ -58,6 +58,7 @@ fn get_parameters (state: &mut ProgramState, pointer: i64, number: (i64, i64), m
     values
 }
 
+#[derive(Default)]
 pub struct ProgramState {
     pub tape: Vec<i64>,
     pub input: Vec<i64>,
@@ -66,65 +67,99 @@ pub struct ProgramState {
     pub relative_base: i64
 }
 
+impl ProgramState {
+    fn next_opcode (&self) -> i64 {
+        self.tape[convert_pointer(self.tape_index)] % 100
+    }
+
+    fn needs_input (&self) -> bool {
+        self.next_opcode() == 3 && self.input_index >= self.input.len()
+    }
+
+    fn is_done (&self) -> bool {
+        self.next_opcode() == 99
+    }
+}
+
+fn do_instruction (state: &mut ProgramState) -> Option<i64> {
+    let instruction = get_value(state, state.tape_index, 1);
+    state.tape_index += 1;
+
+    let opcode = instruction % 100;
+    let modes = instruction / 100;
+
+    // number of (values, pointers)
+    let parameters = match opcode {
+        1 | 2 => (2, 1),
+        3 => (0, 1),
+        4 => (1, 0),
+        5 | 6 => (2, 0),
+        7 | 8 => (2, 1),
+        9 => (1, 0),
+        99 | _ => (0, 0)
+    };
+
+    let (values, pointers) = get_parameters(state, state.tape_index, parameters, modes);
+    state.tape_index += parameters.0 + parameters.1;
+
+    match opcode {
+        1 => {
+            state.tape[pointers[0]] = values[0] + values[1]
+        },
+        2 => {
+            state.tape[pointers[0]] = values[0] * values[1]
+        },
+        3 => {
+            state.tape[pointers[0]] = state.input[state.input_index];
+            state.input_index += 1
+        },
+        4 => {
+            return Some(values[0])
+        },
+        5 => {
+            if values[0] != 0 {
+                state.tape_index = values[1]
+            }
+        },
+        6 => {
+            if values[0] == 0 {
+                state.tape_index = values[1]
+            }
+        },
+        7 => {
+            state.tape[pointers[0]] = (values[0] < values[1]) as i64
+        },
+        8 => {
+            state.tape[pointers[0]] = (values[0] == values[1]) as i64
+        },
+        9 => {
+            state.relative_base += values[0]
+        },
+        99 => {},
+        _ => panic!("unkown opcode")
+    }
+
+    None
+}
+
 pub fn step (state: &mut ProgramState) -> Option<i64> {
-    loop {
-        let instruction = get_value(state, state.tape_index, 1);
-        state.tape_index += 1;
+    let mut output = None;
+    while output.is_none() && !state.is_done() {
+        output = do_instruction(state);
+    }
+    output
+}
 
-        let opcode = instruction % 100;
-        let modes = instruction / 100;
-
-        // (values, pointers)
-        let parameters = match opcode {
-            1 | 2 => (2, 1),
-            3 => (0, 1),
-            4 => (1, 0),
-            5 | 6 => (2, 0),
-            7 | 8 => (2, 1),
-            9 => (1, 0),
-            99 | _ => (0, 0)
-        };
-
-        let (values, pointers) = get_parameters(state, state.tape_index, parameters, modes);
-        state.tape_index += parameters.0 + parameters.1;
-
-        match opcode {
-            1 => {
-                state.tape[pointers[0]] = values[0] + values[1]
-            },
-            2 => {
-                state.tape[pointers[0]] = values[0] * values[1]
-            },
-            3 => {
-                state.tape[pointers[0]] = state.input[state.input_index];
-                state.input_index += 1
-            },
-            4 => {
-                return Some(values[0])
-            },
-            5 => {
-                if values[0] != 0 {
-                    state.tape_index = values[1]
-                }
-            },
-            6 => {
-                if values[0] == 0 {
-                    state.tape_index = values[1]
-                }
-            },
-            7 => {
-                state.tape[pointers[0]] = (values[0] < values[1]) as i64
-            },
-            8 => {
-                state.tape[pointers[0]] = (values[0] == values[1]) as i64
-            },
-            9 => {
-                state.relative_base += values[0]
-            },
-            99 => return None,
-            _ => panic!("unkown opcode")
+pub fn step_input (state: &mut ProgramState, input: Option<i64>) -> Vec<i64> {
+    if input.is_some() { state.input.push(input.unwrap()); }
+    let mut output = Vec::new();
+    while !state.needs_input() && !state.is_done() {
+        let result = do_instruction(state);
+        if result.is_some() {
+            output.push(result.unwrap());
         }
     }
+    output
 }
 
 pub fn run (intcode: &Vec<i64>, input: &Vec<i64>) -> Vec<i64> {
